16a9bd0225ab01864240712a53129a823505aae0
Jazz <jazz@Jazzs-MacBook-Air.local>
2017-09-21 23:59:32 -1000

    contest 2

--- HPUPractiseContest/Contest3/CharacterDevelopment.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-
-class CharacterDevelopment {
-    public static void main(String[] args) {
-        int k = new Scanner(System.in).nextInt();
-        System.out.println((int)Math.pow(2, k) - k - 1);
-    }
-}
