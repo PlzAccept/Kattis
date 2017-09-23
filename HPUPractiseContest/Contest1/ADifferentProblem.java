f69da165953f748e3ac7af3af84a45a178e6eea9
Jazz <jazz@Jazzs-MacBook-Air.local>
2017-09-22 18:05:35 -1000

    Contest 2

--- HPUPractiseContest/Contest1/ADifferentProblem.java
+++ HPUPractiseContest/Contest1/ADifferentProblem.java
@@ -5,7 +5,7 @@ import java.util.*;
 //This is the only tricky part.
 
 class ADifferentProblem {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
 		//read until EOF
